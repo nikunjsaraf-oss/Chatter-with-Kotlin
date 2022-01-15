bb154b9ff7138b077c53e18fe52d6d09da455395
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2022-01-16 00:12:46 +0530

    Finish

---
+++ app/src/main/java/com/nsaraf/chatter_kotlin/Message.kt
@@ -0,0 +1,12 @@
+package com.nsaraf.chatter_kotlin
+
+class Message {
+    var message:String? = null
+    var senderId:String? = null
+    constructor(){}
+
+    constructor(message: String?, senderId:String?){
+        this.message = message
+        this.senderId = senderId
+    }
+}
